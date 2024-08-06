06429658dc0a9c8b8ea7941eb6e233f743b4038e
Tatang Kuswandi <55220773+kuswanid@users.noreply.github.com>
2024-08-06 13:46:43 +0700

    Update record.js

--- routes/record.js
+++ routes/record.js
@@ -11,10 +11,10 @@ const multer = Multer({
 
 // TODO: Sesuaikan konfigurasi database
 const connection = mysql.createConnection({
-    host: 'public_ip_sql_instance_Anda',
+    host: '34.50.64.36',
     user: 'root',
-    database: 'nama_database_Anda',
-    password: 'password_sql_Anda'
+    database: 'money-tracker-db',
+    password: 'qwerty'
 })
 
 router.get("/dashboard", (req, res) => {
